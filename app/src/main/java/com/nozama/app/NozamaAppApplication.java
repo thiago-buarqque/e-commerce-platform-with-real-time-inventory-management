87c27f3dc90044b62b7f627552bc0aa150c47189
Thiago Buarque <thiago.buarqque@gmail.com>
2025-03-04 16:42:58 -0300

    GIR-26 add base maven-java project

---
+++ app/src/main/java/com/nozama/app/NozamaAppApplication.java
@@ -0,0 +1,13 @@
+package com.nozama.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NozamaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NozamaAppApplication.class, args);
+	}
+
+}
