11a272610cca4761699cad766f6da9b6a53de9fb
Thiago Buarque <thiago.buarqque@gmail.com>
2025-03-04 16:42:58 -0300

    GIR-26 add base maven-java project

---
+++ app/src/test/java/com/nozama/app/NozamaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nozama.app;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class NozamaAppApplicationTests {
+
+	@Test
+	void test() {
+		Assert.assertFalse(false);
+	}
+
+}
