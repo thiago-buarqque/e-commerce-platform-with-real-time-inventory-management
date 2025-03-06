7f112ce4b2b87ab3853198e723eb05bd71998192
Thiago Buarque <thiago.buarqque@gmail.com>
2025-03-06 16:14:09 -0300

    GIR-29 add Docker support for dev and production

--- app/src/main/java/com/nozama/app/controller/ProductController.java
+++ app/src/main/java/com/nozama/app/controller/ProductController.java
@@ -1,4 +1,14 @@
 package com.nozama.app.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
 public class ProductController {
+
+    @GetMapping()
+    public String hello() {
+        return "Hello World";
+    }
+
 }
