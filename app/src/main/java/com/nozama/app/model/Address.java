93a58058ed5fe5442d791e41cd1b5f5d5da55d4c
Camile Marcele <mcamileparaujo@gmail.com>
2025-03-15 22:14:34 -0300

    Creating user entity and database connection

---
+++ app/src/main/java/com/nozama/app/model/Address.java
@@ -0,0 +1,23 @@
+package com.nozama.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "Address")
+@NoArgsConstructor
+public class Address {
+    @Id
+    private long id;
+    private String street;
+    private String city;
+    private String state;
+    private String cep;
+    private String country;
+    private int number;
+
+}
