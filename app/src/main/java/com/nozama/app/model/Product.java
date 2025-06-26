db5a35410d753a013b68a2736324cbc30b973595
camimcl <mcamileparaujo@gmail.com>
2025-06-26 18:51:50 -0300

    Product entity

--- app/src/main/java/com/nozama/app/model/Product.java
+++ app/src/main/java/com/nozama/app/model/Product.java
@@ -1,3 +1,33 @@
 package com.nozama.app.model;
 
-public class Product {}
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+    
+    private String description;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private Integer stockQuantity;
+}
