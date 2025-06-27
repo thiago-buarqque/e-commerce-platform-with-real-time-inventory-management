ab71f73b1ff328fb8ba8392c0f15c8a254d8075f
Thiago Buarque <thiago.buarqque@gmail.com>
2025-06-27 00:33:04 -0300

    disable csrf

--- app/src/main/java/com/nozama/app/configuration/SecurityConfiguration.java
+++ app/src/main/java/com/nozama/app/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,7 +19,9 @@ public class SecurityConfiguration {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http.authorizeHttpRequests(
+    return http
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
             (authorize) ->
                 authorize.requestMatchers("/login").permitAll().anyRequest().authenticated())
         .httpBasic(Customizer.withDefaults())
--- app/src/main/java/com/nozama/app/controller/ProductController.java
+++ app/src/main/java/com/nozama/app/controller/ProductController.java
@@ -4,16 +4,20 @@ import com.nozama.app.dto.ProductRequest;
 import com.nozama.app.dto.ProductResponse;
 import com.nozama.app.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/products")
+@RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
 
@@ -21,15 +25,17 @@ public class ProductController {
 
   @PreAuthorize("hasRole('ADMIN')")
   @PostMapping
-  public ResponseEntity<ProductResponse> createProduct (@RequestBody ProductRequest request){
-    return new ResponseEntity<>(productService.createProduct(request), HttpStatus.CREATED);
+  public ProductResponse createProduct(@RequestBody ProductRequest request){
+    return productService.createProduct(request);
   }
-  public ResponseEntity<List<ProductResponse>> searchProducts(
+
+  @GetMapping
+  public List<ProductResponse> searchProducts(
           @RequestParam(required = false) String search,
           @RequestParam(required = false) String category,
           @RequestParam(required = false) BigDecimal minPrice,
           @RequestParam(required = false) BigDecimal maxPrice
   ) {
-    return ResponseEntity.ok(productService.searchProducts(search, category, minPrice, maxPrice));
+    return productService.searchProducts(search, category, minPrice, maxPrice);
   }
 }
