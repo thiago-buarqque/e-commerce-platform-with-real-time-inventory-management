57012c31a5204e2b09744acaa78b862ce8358a23
camimcl <mcamileparaujo@gmail.com>
2025-07-02 19:56:12 -0300

    adding depends_on to docker-compose

--- app/src/main/java/com/nozama/app/exception/ValidationExceptionHandler.java
+++ app/src/main/java/com/nozama/app/exception/ValidationExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.nozama.app.exception;
 
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
