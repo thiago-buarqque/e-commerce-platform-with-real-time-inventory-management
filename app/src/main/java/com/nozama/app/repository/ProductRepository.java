9b1c41defd9f12c88839a13fdfeab649b6dcc7e1
camimcl <mcamileparaujo@gmail.com>
2025-07-02 20:07:58 -0300

    adding product sorting and pagination

--- app/src/main/java/com/nozama/app/repository/ProductRepository.java
+++ app/src/main/java/com/nozama/app/repository/ProductRepository.java
@@ -1,7 +1,30 @@
 package com.nozama.app.repository;
 
 import com.nozama.app.model.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import java.awt.print.Pageable;
+import java.math.BigDecimal;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    @Query("""
+        SELECT p FROM Product p
+        WHERE (:search IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :search, '%')) OR LOWER(p.description) LIKE LOWER(CONCAT('%', :search, '%')))
+          AND (:category IS NULL OR LOWER(p.category) = LOWER(:category))
+          AND (:minPrice IS NULL OR p.price >= :minPrice)
+          AND (:maxPrice IS NULL OR p.price <= :maxPrice)
+    """)
+    Page<Product> findByFilters(
+            @Param("search") String search,
+            @Param("category") String category,
+            @Param("minPrice") BigDecimal minPrice,
+            @Param("maxPrice") BigDecimal maxPrice,
+            Pageable pageable
+    );
 }
--- app/src/main/java/com/nozama/app/service/ProductService.java
+++ app/src/main/java/com/nozama/app/service/ProductService.java
@@ -2,11 +2,20 @@ package com.nozama.app.service;
 
 import com.nozama.app.dto.ProductRequest;
 import com.nozama.app.dto.ProductResponse;
+import org.springframework.data.domain.Page;
 
+import java.awt.print.Pageable;
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductService {
     ProductResponse createProduct (ProductRequest request);
-    List<ProductResponse> searchProducts(String search, String category, BigDecimal minPrice, BigDecimal maxPrice);
+
+    Page<ProductResponse> searchProducts(
+            String search,
+            String category,
+            BigDecimal minPrice,
+            BigDecimal maxPrice,
+            Pageable pageable
+    );
 }
