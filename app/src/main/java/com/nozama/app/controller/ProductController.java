92edcaeedad6346b82bfc6818c08d1135f6a5ed1
camimcl <mcamileparaujo@gmail.com>
2025-07-02 23:06:13 -0300

    @Query and Pageable error fixing

--- app/src/main/java/com/nozama/app/controller/ProductController.java
+++ app/src/main/java/com/nozama/app/controller/ProductController.java
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.math.BigDecimal;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/products")
@@ -39,7 +39,7 @@ public class ProductController {
           @RequestParam(required = false) String category,
           @RequestParam(required = false) BigDecimal minPrice,
           @RequestParam(required = false) BigDecimal maxPrice,
-          @PageableDefault(page = 0, size = 10, sort = "name") Pageable pageable
+          @PageableDefault(page = 0, size = 3, sort = "name") Pageable pageable
   ) {
     return productService.searchProducts(search, category, minPrice, maxPrice, pageable);
   }
