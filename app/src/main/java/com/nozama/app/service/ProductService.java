70a517f2cf4a63b0700a526eb3569e999ff9b838
camimcl <mcamileparaujo@gmail.com>
2025-07-02 23:06:13 -0300

    @Query and Pageable error fixing

--- app/src/main/java/com/nozama/app/service/ProductService.java
+++ app/src/main/java/com/nozama/app/service/ProductService.java
@@ -4,9 +4,8 @@ import com.nozama.app.dto.ProductRequest;
 import com.nozama.app.dto.ProductResponse;
 import org.springframework.data.domain.Page;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.math.BigDecimal;
-import java.util.List;
 
 public interface ProductService {
     ProductResponse createProduct (ProductRequest request);
