610873da9bb212c7ab96814853c741d1bc9f362f
camimcl <mcamileparaujo@gmail.com>
2025-08-04 16:57:34 -0300

    adding user creation,deletion,search and update

--- app/src/main/java/com/nozama/app/dto/UserRequest.java
+++ app/src/main/java/com/nozama/app/dto/UserRequest.java
@@ -1,10 +1,15 @@
 package com.nozama.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserRequest {
+    private long id;
     private String name;
     private String email;
     private String password;
+    private String phone;
 }
--- app/src/main/java/com/nozama/app/model/User.java
+++ app/src/main/java/com/nozama/app/model/User.java
@@ -1,6 +1,9 @@
 package com.nozama.app.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,7 +29,9 @@ public class User {
   private long id;
 
   private String name;
+  @Column(unique = true)
   private String email;
+
   private String password;
   private String cpf;
   private String phone;
