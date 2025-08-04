93f5588505ff56ee1dbe7684930a7f7bd29320ad
camimcl <mcamileparaujo@gmail.com>
2025-08-03 21:09:35 -0300

    adding product controller

--- app/src/main/java/com/nozama/app/dto/OrderRequest.java
+++ app/src/main/java/com/nozama/app/dto/OrderRequest.java
@@ -1,5 +1,8 @@
 package com.nozama.app.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -8,9 +11,16 @@ import java.util.List;
 
 @Data
 public class OrderRequest {
+    @NotNull
     private Long userId;
+
     private List<OrderItemRequest> items;
+    @NotNull
     private String paymentMethod;
+
+    @NotNull
+    @Positive
     private BigDecimal totalAmount;
+    @PastOrPresent
     private LocalDateTime CreatedAt;
 }
